140c5b628d6eb3d41381d37494f202c1ce770b03
Regina Naa Dedei Crabbe <rndcrabbe@gmail.com>
2023-10-27 13:44:33 +0000

    add home

--- src/Home.py
+++ src/Home.py
@@ -1,5 +1,6 @@
-# Load libraries needed
+#Load libraries needed
 import streamlit as st
+ 
  
 # Set page configuration
 st.set_page_config(
@@ -8,41 +9,69 @@ st.set_page_config(
     layout="wide"
 )
  
-# Create a sidebar to select pages
-page = st.sidebar.radio("Select a page:", ("Home", "Forecast Sales", "View Dataset", "About Team"))
+# Add content to your Streamlit app
+st.markdown("# 👋 Welcome To Team Cape Cod's Sales Forecasting App Of Products Across Favorita stores")
  
-# Add content to your Streamlit app based on the selected page
-if page == "Home":
-    st.markdown("# 👋 Welcome To Team Cape Cod's Sales Forecasting App Of Products Across Favorita stores")
-    st.image("https://www.animatedimages.org/data/media/1645/animated-waving-image-0090.gif")
-    st.write("""
+# Display the waving GIF
+st.image("https://www.animatedimages.org/data/media/1645/animated-waving-image-0090.gif")
+ 
+# Add CSS for animation
+st.write("""
+    <style>
+        @keyframes slide-in {
+            0% {
+                transform: translateX(-100%);
+            }
+            100% {
+                transform: translateX(0);
+            }
+        }
+        .slide-in-animation {
+            animation: slide-in 1.5s ease-in-out;
+        }
+    </style>
+""", unsafe_allow_html=True)
+ 
+ 
+# Text with animation
+st.write('<div class="slide-in-animation">Favorita Corporation is an Ecuador based company engaged in the organization, installation and administration of stores, markets and supermarkets. The company has a business presence in many international countries also. This app with the help of a linear regression model will enable you to predict sales across Favorita Stores.</div>', unsafe_allow_html=True)
+ 
+#add a sidebar to select pages
+st.sidebar.success("Select a page above.")
+ 
+ 
+ 
+# Create a Streamlit container for the subheader
+subheader_container = st.container()
+ 
+# Define the subheader content
+subheader_content = """
+<div class="slide-in-animation">
+<h3>Things You Can Do On This App:</h3>
+<ul>
+  <li>Forecast Sales of a Specific Date for Favorita Store</li>
+  <li>View the dataset and interact with a visual showing daily sales across stores</li>
+  <li>Get to know more about the team</li>
+</ul>
+</div>
+"""
+ 
+# Apply CSS animation using HTML/CSS
+subheader_container.markdown(subheader_content, unsafe_allow_html=True)
+ 
+# Add CSS for animation
+st.write("""
 <style>
-            @keyframes slide-in {
-                0% {
-                    transform: translateX(-100%);
-                }
-                100% {
-                    transform: translateX(0);
-                }
-            }
-            .slide-in-animation {
-                animation: slide-in 1.5s ease-in-out;
-            }
+    @keyframes slide-in {
+        0% {
+            transform: translateX(-100%);
+        }
+        100% {
+            transform: translateX(0);
+        }
+    }
+    .slide-in-animation {
+        animation: slide-in 1.5s ease;
+    }
 </style>
-    """, unsafe_allow_html=True)
-    st.markdown('<div class="slide-in-animation">Favorita Corporation is an Ecuador-based company engaged in the organization, installation, and administration of stores, markets, and supermarkets. The company has a business presence in many international countries also. This app, with the help of a linear regression model, will enable you to predict sales across Favorita Stores.</div>', unsafe_allow_html=True)
- 
-if page == "Forecast":
-    # Add content for the "Forecast Sales" page
-    st.markdown("## Forecast Sales Page Content")
-    # Add your content for this page here
- 
-if page == "Explore":
-    # Add content for the "View Dataset" page
-    st.markdown("## View Dataset Page Content")
-    # Add your content for this page here
- 
-if page == "About":
-    # Add content for the "About Team" page
-    st.markdown("## About Team Page Content")
-    # Add your content for this page here
+""", unsafe_allow_html=True)
